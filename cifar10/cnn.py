6baf0c3efbf13c9b20d64110c5ea3f0f7ec4ed51
caiweiesat <2528321175@qq.com>
2026-07-11 07:04:37 +0800

    可成功运行多组实验，后续可能需要完善损失曲线，比如在一张图内画出训练集和测试集的损失曲线

--- cifar10/cnn.py
+++ cifar10/cnn.py
@@ -47,3 +47,50 @@ class MyNet(nn.Module):
         x=self.main(x)
         x=self.fc(x)
         return x
+
+class MyNetSingleColor(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.main=nn.Sequential(
+            nn.Conv2d(1,32,kernel_size=3,padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+
+            nn.Conv2d(32,64,kernel_size=3,padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+
+            nn.Conv2d(64,128,kernel_size=3,padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+
+            # nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(256),
+            # nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.fc=nn.Sequential(
+            nn.Flatten(),
+
+            nn.Linear(128*4*4,1024),
+            nn.ReLU(),
+            nn.Dropout(),
+
+            nn.Linear(1024,10),
+            # nn.ReLU(),
+            # nn.Dropout(),
+            #
+            # nn.Linear(256, 128),
+            # nn.ReLU(),
+            # nn.Dropout(),
+            #
+            # nn.Linear(128,10),
+
+        )
+    def forward(self,x):
+        x=self.main(x)
+        x=self.fc(x)
+        return x
--- cifar10/my_train.py
+++ cifar10/my_train.py
@@ -1,12 +1,12 @@
-import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, TensorDataset
 
-from cnn import *
-import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
-
-from approximate import *
+# 以下是自定义的py文件
+from cnn import *
+from read import *
+from device_test_data import *
+from simulation_approximation import *
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -21,155 +21,249 @@ meta_path = r"D:\PythonProject\cifar10\cifar-10-batches-py\batches.meta"
 meta_data = unpickle(meta_path)
 class_names = meta_data["label_names"]
 
-apx_x_train = x_train.copy()
-apx_x_test = x_test.copy()
-"""R"""
-R_all_gray = gray_combination(energy_density=R_energy_density,
-                              polarized_light=R_polarized_light,
-                              energy_benchmark=R_energy_benchmark,
-                              slope=R_slope)
+"""
+一共6组实验
+RGB 能量密度+偏振角
+RGB 能量密度
+RGB 偏振角
+R   能量密度+偏振角
+G   能量密度+偏振角
+B   能量密度+偏振角
+"""
 
-apx_x_train = gray_apx(apx_x_train, R_all_gray, color="R")
-apx_x_test = gray_apx(apx_x_test, R_all_gray, color="R")
+#先获取R、G、B的单能量、单偏振角、能量和偏振角组合的可调灰度值
+# 只有能量密度
+R_energy_gray=gray_combination(energy_density=R_energy_density)
+G_energy_gray=gray_combination(energy_density=G_energy_density)
+B_energy_gray=gray_combination(energy_density=B_energy_density)
+# 只有偏振角
+R_polarized_gray=gray_combination(polarized_light=R_polarized_light)
+G_polarized_gray=gray_combination(polarized_light=G_polarized_light)
+B_polarized_gray=gray_combination(polarized_light=B_polarized_light)
+# 能量密度和偏振角组合
+R_energy_and_polarized_gray = gray_combination(energy_density=R_energy_density,
+                                               polarized_light=R_polarized_light,
+                                               energy_benchmark=R_energy_benchmark,
+                                               slope=R_slope)
+G_energy_and_polarized_gray = gray_combination(energy_density=G_energy_density,
+                                               polarized_light=G_polarized_light,
+                                               energy_benchmark=G_energy_benchmark,
+                                               slope=G_slope)
+B_energy_and_polarized_gray = gray_combination(energy_density=B_energy_density,
+                                               polarized_light=B_polarized_light,
+                                               energy_benchmark=B_energy_benchmark,
+                                               slope=B_slope)
 
-"""G"""
-G_all_gray = gray_combination(energy_density=G_energy_density,
-                              polarized_light=G_polarized_light,
-                              energy_benchmark=G_energy_benchmark,
-                              slope=G_slope)
+# 标准化参数
+norm = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+# 然后获取RGB三种情况下的数据集，单通道的数据集由RGB通道数据集做切片获得
+# RGB 能量密度
+RGB_energy_x_train = x_train.copy()
+RGB_energy_x_test = x_test.copy()
 
-apx_x_train = gray_apx(apx_x_train, G_all_gray, color="G")
-apx_x_test = gray_apx(apx_x_test, G_all_gray, color="G")
+RGB_energy_x_train = gray_apx(RGB_energy_x_train, R_energy_gray, color="R")
+RGB_energy_x_train = gray_apx(RGB_energy_x_train, G_energy_gray, color="G")
+RGB_energy_x_train = gray_apx(RGB_energy_x_train, B_energy_gray, color="B")
 
-"""B"""
-B_all_gray = gray_combination(energy_density=B_energy_density,
-                              polarized_light=B_polarized_light,
-                              energy_benchmark=B_energy_benchmark,
-                              slope=B_slope)
-
-apx_x_train = gray_apx(apx_x_train, B_all_gray, color="B")
-apx_x_test = gray_apx(apx_x_test, B_all_gray, color="B")
+RGB_energy_x_test = gray_apx(RGB_energy_x_test, R_energy_gray, color="R")
+RGB_energy_x_test = gray_apx(RGB_energy_x_test, G_energy_gray, color="G")
+RGB_energy_x_test = gray_apx(RGB_energy_x_test, B_energy_gray, color="B")
 
 # 维度重排+转float+归一化
-train_x_tensor = torch.from_numpy(apx_x_train).permute(0, 3, 1, 2).float() / 255.0
-test_x_tensor = torch.from_numpy(apx_x_test).permute(0, 3, 1, 2).float() / 255.0
+RGB_energy_x_train_tensor = torch.from_numpy(RGB_energy_x_train).permute(0,3,1,2).float()/255.0
+RGB_energy_x_test_tensor = torch.from_numpy(RGB_energy_x_test).permute(0,3,1,2).float()/255.0
+# 标准化
+RGB_energy_x_train_tensor = norm(RGB_energy_x_train_tensor)
+RGB_energy_x_test_tensor = norm(RGB_energy_x_test_tensor)
 
+# RGB 偏振角
+RGB_polarized_x_train = x_train.copy()
+RGB_polarized_x_test = x_test.copy()
+
+RGB_polarized_x_train = gray_apx(RGB_polarized_x_train, R_polarized_gray, color="R")
+RGB_polarized_x_train = gray_apx(RGB_polarized_x_train, G_polarized_gray, color="G")
+RGB_polarized_x_train = gray_apx(RGB_polarized_x_train, B_polarized_gray, color="B")
+
+RGB_polarized_x_test = gray_apx(RGB_polarized_x_test, R_polarized_gray, color="R")
+RGB_polarized_x_test = gray_apx(RGB_polarized_x_test, G_polarized_gray, color="G")
+RGB_polarized_x_test = gray_apx(RGB_polarized_x_test, B_polarized_gray, color="B")
+
+# 维度重排+转float+归一化
+RGB_polarized_x_train_tensor = torch.from_numpy(RGB_polarized_x_train).permute(0,3,1,2).float()/255.0
+RGB_polarized_x_test_tensor = torch.from_numpy(RGB_polarized_x_test).permute(0,3,1,2).float()/255.0
+# 标准化
+RGB_polarized_x_train_tensor = norm(RGB_polarized_x_train_tensor)
+RGB_polarized_x_test_tensor = norm(RGB_polarized_x_test_tensor)
+
+# RGB 能量密度+偏振角
+RGB_energy_and_polarized_x_train = x_train.copy()
+RGB_energy_and_polarized_x_test = x_test.copy()
+
+RGB_energy_and_polarized_x_train = gray_apx(RGB_energy_and_polarized_x_train, R_energy_and_polarized_gray, color="R")
+RGB_energy_and_polarized_x_train = gray_apx(RGB_energy_and_polarized_x_train, G_energy_and_polarized_gray, color="G")
+RGB_energy_and_polarized_x_train = gray_apx(RGB_energy_and_polarized_x_train, B_energy_and_polarized_gray, color="B")
+
+RGB_energy_and_polarized_x_test = gray_apx(RGB_energy_and_polarized_x_test, R_energy_and_polarized_gray, color="R")
+RGB_energy_and_polarized_x_test = gray_apx(RGB_energy_and_polarized_x_test, G_energy_and_polarized_gray, color="G")
+RGB_energy_and_polarized_x_test = gray_apx(RGB_energy_and_polarized_x_test, B_energy_and_polarized_gray, color="B")
+
+# 维度重排+转float+归一化
+RGB_energy_and_polarized_x_train_tensor = torch.from_numpy(RGB_energy_and_polarized_x_train).permute(0,3,1,2).float()/255.0
+RGB_energy_and_polarized_x_test_tensor = torch.from_numpy(RGB_energy_and_polarized_x_test).permute(0,3,1,2).float()/255.0
+# 标准化
+RGB_energy_and_polarized_x_train_tensor = norm(RGB_energy_and_polarized_x_train_tensor)
+RGB_energy_and_polarized_x_test_tensor = norm(RGB_energy_and_polarized_x_test_tensor)
+
+# R 能量密度+偏振角
+R_energy_and_polarized_x_train_tensor = RGB_energy_and_polarized_x_train_tensor[:,0:1,:,:]
+R_energy_and_polarized_x_test_tensor = RGB_energy_and_polarized_x_test_tensor[:,0:1,:,:]
+# G 能量密度+偏振角
+G_energy_and_polarized_x_train_tensor = RGB_energy_and_polarized_x_train_tensor[:,1:2,:,:]
+G_energy_and_polarized_x_test_tensor = RGB_energy_and_polarized_x_test_tensor[:,1:2,:,:]
+# B 能量密度+偏振角
+B_energy_and_polarized_x_train_tensor = RGB_energy_and_polarized_x_train_tensor[:,2:3,:,:]
+B_energy_and_polarized_x_test_tensor = RGB_energy_and_polarized_x_test_tensor[:,2:3,:,:]
+"""
+一共6组实验
+RGB 能量密度+偏振角
+RGB 能量密度
+RGB 偏振角
+R   能量密度+偏振角
+G   能量密度+偏振角
+B   能量密度+偏振角
+"""
+# 实验数据tuple用来控制循环
+research_data_tuple=((RGB_energy_and_polarized_x_train_tensor,RGB_energy_and_polarized_x_test_tensor),
+                     (RGB_energy_x_train_tensor,RGB_energy_x_test_tensor),
+                     (RGB_polarized_x_train_tensor,RGB_polarized_x_test_tensor),
+                     (R_energy_and_polarized_x_train_tensor,R_energy_and_polarized_x_test_tensor),
+                     (G_energy_and_polarized_x_train_tensor,G_energy_and_polarized_x_test_tensor),
+                     (B_energy_and_polarized_x_train_tensor,B_energy_and_polarized_x_test_tensor))
+# 每项实验共用的标签张量
 train_y_tensor = torch.from_numpy(y_train).long()
 test_y_tensor = torch.from_numpy(y_test).long()
-# 标准化
-norm = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-train_x_tensor = norm(train_x_tensor)
-test_x_tensor = norm(test_x_tensor)
-# 数据集
-train_set = TensorDataset(train_x_tensor, train_y_tensor)
-test_set = TensorDataset(test_x_tensor, test_y_tensor)
-# 数据加载器
-train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_set, batch_size=64, shuffle=False)
+for experiment_number,(train_x_tensor,test_x_tensor) in enumerate(research_data_tuple):
+    # 数据集
+    train_set = TensorDataset(train_x_tensor, train_y_tensor)
+    test_set = TensorDataset(test_x_tensor, test_y_tensor)
+    # 数据加载器
+    train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=64, shuffle=False)
 
-model = MyNet()
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    if train_x_tensor.shape[1] == 3:
+        model = MyNet()
+    elif train_x_tensor.shape[1] == 1:
+        model = MyNetSingleColor()
+    else:
+        raise ValueError(".shape[1]的值应是3或1")
 
-# 损失list
-loss_list = []
-#正确率list
-correct_list = []
-epochs = 30
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-for i in range(epochs + 1):
-    if i == 0:  # 第0轮计算loss和正确率
-        # loss
-        model.train()
-        epoch_loss = 0
-        with torch.no_grad():
+
+    # 损失list
+    loss_list = []
+    #正确率list
+    correct_list = []
+    epochs = 1
+
+    for i in range(epochs + 1):
+        if i == 0:  # 第0轮计算loss和正确率
+            # loss
+            model.train()
+            epoch_loss = 0
+            with torch.no_grad():
+                for images, label in train_loader:
+                    pred = model(images)
+                    loss = criterion(pred, label)
+
+                    epoch_loss += loss.item()
+                # 训练集损失
+                avg_loss = epoch_loss / len(train_loader)
+                loss_list.append(avg_loss)
+            # 正确率
+            model.eval()
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for images, labels in test_loader:
+                    outputs = model(images)
+                    _, pred = torch.max(outputs, dim=1)
+                    total += labels.size(0)
+                    correct += (pred == labels).sum().item()
+            acc = correct / total
+            print(f"第{i}轮正确率{acc:.2f}")
+            correct_list.append(acc)
+        else:  # 每轮训练计算loss和正确率
+            # loss
+            model.train()
+            epoch_loss = 0
             for images, label in train_loader:
                 pred = model(images)
                 loss = criterion(pred, label)
 
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
                 epoch_loss += loss.item()
-            # 训练集损失
+
             avg_loss = epoch_loss / len(train_loader)
             loss_list.append(avg_loss)
-        # 正确率
-        model.eval()
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for images, labels in test_loader:
-                outputs = model(images)
-                _, pred = torch.max(outputs, dim=1)
-                total += labels.size(0)
-                correct += (pred == labels).sum().item()
-        acc = correct / total
-        print(f"第{i}轮正确率{acc:.2f}")
-        correct_list.append(acc)
-    else:  # 每轮训练计算loss和正确率
-        # loss
-        model.train()
-        epoch_loss = 0
-        for images, label in train_loader:
-            pred = model(images)
-            loss = criterion(pred, label)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            # 正确率
+            model.eval()
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for images, labels in test_loader:
+                    outputs = model(images)
+                    _, pred = torch.max(outputs, dim=1)
+                    total += labels.size(0)
+                    correct += (pred == labels).sum().item()
+            acc = correct / total
+            print(f"第{i}轮正确率{acc:.2f}")
+            correct_list.append(acc)
 
-            epoch_loss += loss.item()
 
-        avg_loss = epoch_loss / len(train_loader)
-        loss_list.append(avg_loss)
+    experiment_name=("RGB_能量密度+偏振角",
+                     "RGB_能量密度",
+                     "RGB_偏振角",
+                     "R_能量密度+偏振角",
+                     "G_能量密度+偏振角",
+                     "B_能量密度+偏振角")
+    print(f"{experiment_name[experiment_number]}训练完成")
+    # x轴：1~总轮数
+    x = list(range(0, epochs + 1))
+    # y轴：每轮损失
+    y = loss_list
 
-        # 正确率
-        model.eval()
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for images, labels in test_loader:
-                outputs = model(images)
-                _, pred = torch.max(outputs, dim=1)
-                total += labels.size(0)
-                correct += (pred == labels).sum().item()
-        acc = correct / total
-        print(f"第{i}轮正确率{acc:.2f}")
-        correct_list.append(acc)
+    plt.figure(figsize=(8, 4))
+    plt.plot(x, y, color='red', marker='o', label='Train Loss')
+    # 强制X轴只显示整数
+    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Training Loss Curve")
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.savefig(f"训练集损失曲线{experiment_name[experiment_number]}.png", dpi=300)
+    plt.close()
 
-""""""
-test_num = 5
-""""""
-
-# x轴：1~总轮数
-x = list(range(0, epochs + 1))
-# y轴：每轮损失
-y = loss_list
-
-plt.figure(figsize=(8, 4))
-plt.plot(x, y, color='red', marker='o', label='Train Loss')
-# 强制X轴只显示整数
-plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
-plt.title("Training Loss Curve")
-plt.legend()
-plt.grid(True, alpha=0.3)
-plt.savefig(f"loss_curve{test_num}.png", dpi=300)
-plt.close()
-
-# x轴：1~总轮数
-x = list(range(0, epochs + 1))
-# y轴：每轮正确率
-y = correct_list
-plt.figure(figsize=(8, 4))
-plt.plot(x, y, color='red', marker='o', label='Train Accuracy')
-# 强制X轴只显示整数
-plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
-# 固定Y轴0~1
-plt.ylim(0, 1)
-plt.xlabel("Epoch")
-plt.ylabel("Accuracy")
-plt.title("Training Accuracy Curve")
-plt.legend()
-plt.grid(True, alpha=0.3)
-plt.savefig(f"Accuracy_curve{test_num}.png", dpi=300)
-plt.close()
+    # x轴：1~总轮数
+    x = list(range(0, epochs + 1))
+    # y轴：每轮正确率
+    y = correct_list
+    plt.figure(figsize=(8, 4))
+    plt.plot(x, y, color='red', marker='o', label='Train Accuracy')
+    # 强制X轴只显示整数
+    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+    # 固定Y轴0~1
+    plt.ylim(0, 1)
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.title("Training Accuracy Curve")
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.savefig(f"正确率曲线{experiment_name[experiment_number]}.png", dpi=300)
+    plt.close()
